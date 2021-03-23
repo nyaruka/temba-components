24311383f0fd2ed461f8b786cf0fa46e5d829990
Eric Newcomer <ericnewcomer@gmail.com>
2021-03-23 12:24:55 -0700

    Add screenshot tests for alerts

--- test/temba-alert.test.ts
+++ test/temba-alert.test.ts
@@ -2,17 +2,35 @@ import { fixture, assert } from '@open-wc/testing';
 import { Alert } from '../src/alert/Alert';
 
 import './utils.test';
+import { assertScreenshot, getClip } from './utils.test';
 
-export const getHTML = () => {
-  return `<temba-alert></temba-alert>`;
+export const createAlert = async (level: string, body: string) => {
+  const parentNode = document.createElement('div');
+  parentNode.setAttribute('style', 'width: 250px;');
+  return (await fixture(`<temba-alert level="${level}">${body}</temba-alert>`, {
+    parentNode,
+  })) as Alert;
 };
 
 describe('temba-alert', () => {
   beforeEach(() => {});
   afterEach(() => {});
 
-  it('can be created', async () => {
-    const alert: Alert = await fixture(getHTML());
+  it('shows info', async () => {
+    const alert = await createAlert('info', 'Here is some info');
     assert.instanceOf(alert, Alert);
+    await assertScreenshot('alert/info', getClip(alert));
+  });
+
+  it('shows warning', async () => {
+    const alert = await createAlert('warning', 'Here is a warning');
+    assert.instanceOf(alert, Alert);
+    await assertScreenshot('alert/warning', getClip(alert));
+  });
+
+  it('shows error', async () => {
+    const alert = await createAlert('error', 'Here is an error');
+    assert.instanceOf(alert, Alert);
+    await assertScreenshot('alert/error', getClip(alert));
   });
 });
