97ab421a401cb915c4f29012bfbf8af93e939454
Eric Newcomer <ericnewcomer@gmail.com>
2022-03-17 13:19:52 -0700

    Update screenshots

--- test/temba-contact-history.test.ts
+++ test/temba-contact-history.test.ts
@@ -74,7 +74,7 @@ describe('temba-contact-history', () => {
     // we should have scrolled to the bottom
     const events = history.shadowRoot.querySelector('.events');
     const top = events.scrollHeight - events.getBoundingClientRect().height;
-    expect(top).to.equal(228);
+    expect(top).to.equal(223);
 
     // make sure we actually scrolled to there
     // expect(events.scrollTop).to.equal(top - 1);
