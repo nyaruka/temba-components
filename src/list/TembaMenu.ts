a2d766d2bde1b5049a28227a996f77a28d8dc8dc
Eric Newcomer <ericnewcomer@gmail.com>
2022-04-15 14:23:42 -0700

    Tighten up menu buttons

--- src/list/TembaMenu.ts
+++ src/list/TembaMenu.ts
@@ -394,8 +394,8 @@ export class TembaMenu extends RapidElement {
       }
 
       temba-button {
-        margin-top: 0.5em;
-        margin-bottom: 0.5em;
+        margin-top: 0.2em;
+        margin-bottom: 0.2em;
         margin-left: 0.75em;
         margin-right: 0.75em;
       }
