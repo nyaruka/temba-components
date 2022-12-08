c77dd0436cde047a80f8f22f0517fa37c4e69dd0
susanm74 <susan.m.lister@gmail.com>
2022-12-08 15:08:38 +0700

    component and css cleanup
    - removed modax headers const
    - removed extraneous css properties from each class
    - combined button and primary_button classes (bc they're the same for now)
    - updated div[slot='dropdown'] selector to .dropdown
    - added .item:hover styles
    - converted all colors to rgb(a) for consistency
    - removed primary button component property
    - removed comments above render method
    - added toggle and dropdown classes to slot elements
    - removed extra item else if check for modax item

--- src/list/ContentMenu.ts
+++ src/list/ContentMenu.ts
@@ -9,9 +9,6 @@ const HEADERS = {
   'Temba-Content-Menu': '1',
   'Temba-Spa': '1',
 };
-const MODAX_HEADERS = {
-  'Temba-Spa': '1',
-};
 
 export class ContentMenu extends RapidElement {
   static get styles() {
@@ -20,43 +17,35 @@ export class ContentMenu extends RapidElement {
         --button-y: 0.4em;
         --button-x: 1em;
         display: flex;
-        height: 100%;
       }
 
+      .button_item,
       .primary_button_item {
         margin-left: 1rem;
       }
 
-      .button_item {
-        margin-left: 1rem;
-      }
-
-      temba-dropdown {
-        align-items: stretch;
-        display: flex;
-        flex-direction: column;
-      }
-
       .toggle {
-        color: #666;
-        align-items: center;
-        display: flex;
-        flex-direction: column;
+        --icon-color: rgb(102, 102, 102);
         padding: 0.5rem;
         margin-left: 0.5rem;
-        border-radius: var(--curvature);
       }
 
       .toggle:hover {
-        background: yellow;
-        color: purple;
+        background: rgba(0, 0, 0, 0.05);
+        border-radius: var(--curvature);
+        --icon-color: rgb(136, 136, 136);
       }
 
-      div[slot='dropdown'] {
+      .dropdown {
         padding: 1rem 1.5rem;
         color: rgb(45, 45, 45);
         z-index: 50;
         min-width: 200px;
+      }
+
+      .divider {
+        border-bottom: 1px solid rgb(237, 237, 237);
+        margin: 1rem -1.5em;
       }
 
       .item {
@@ -64,22 +53,17 @@ export class ContentMenu extends RapidElement {
         margin: 0.2em 0em;
         font-size: 1.1rem;
         cursor: pointer;
-        color: inherit;
         font-weight: 400;
       }
 
-      .divider {
-        border-bottom: 1px solid #ccc;
-        margin: 1rem -1.5em;
+      .item:hover {
+        color: rgb(var(--primary-rgb));
       }
     `;
   }
 
   @property({ type: String })
   endpoint: string;
-
-  // @property({ type: ContentMenuItem})
-  // primaryButton: ContentMenuItem;
 
   @property({ type: Array })
   buttons: ContentMenuItem[] = [];
@@ -97,8 +81,8 @@ export class ContentMenu extends RapidElement {
     getUrl(this.endpoint, null, HEADERS)
       .then((response: WebResponse) => {
         const json = response.json;
+        // console.log('items', json.items);
         const contentMenu = json.items as ContentMenuItem[];
-        // this.primaryButton = contentMenu.filter(item => item.as_button && item.primary); //todo do we need this?
         this.buttons = contentMenu.filter(item => item.as_button);
         this.items = contentMenu.filter(item => !item.as_button);
       })
@@ -121,14 +105,6 @@ export class ContentMenu extends RapidElement {
     this.fireCustomEvent(CustomEventType.Selection, item);
   }
 
-  //todo div container class needs to transcribe -> .flex.h-full.spa-gear-buttons
-  //todo temba-button class needs to transcribe -> .ml-4
-  //todo temba-dropdown class needs to transcribe -> .items-stretch.flex.flex-column(arrowoffset="-12" offsetx="24" offsety="6" arrowsize="8")
-  //todo div slot="toggle" class needs to transcribe -> .menu-button.items-center.flex.flex-column.py-2.px-2.ml-2
-  //todo div slot="dropdown" class needs to transcribe -> .px-6.py-4.text-gray-800.z-50 and style="min-width:200px"
-  //todo div dropdown divider item class needs to transcribe -> .border-b.border-gray-200.my-4.-mx-6
-  //todo div dropdown regular item class needs to transcribe -> .whitespace-nowrap.menu-item.hover-linked.font-normal
-  //todo what is gear-flag and item.flag? spa_page_menu.aml line 157
   public render(): TemplateResult {
     return html`
       <div class="container">
@@ -146,23 +122,14 @@ export class ContentMenu extends RapidElement {
           arrowoffset="-12"
           offsetx="24"
           offsety="6"
-          class="TODO"
         >
-          <div class="toggle" slot="toggle">
+          <div slot="toggle" class="toggle">
             <temba-icon name="menu" size="1.5" />
           </div>
-          <div slot="dropdown">
+          <div slot="dropdown" class="dropdown">
             ${this.items.map(item => {
               if (item.type === 'divider') {
                 return html` <div class="divider"></div>`;
-              } else if (item.type === 'modax') {
-                return html` <div
-                  class="item"
-                  name=${item.label}
-                  @click=${() => this.handleItemClicked(item)}
-                >
-                  ${item.label}
-                </div>`;
               } else {
                 return html` <div
                   class="item"
