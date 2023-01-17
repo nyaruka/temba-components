dd8b5b9b7e245842e3202f2918c94c7c5acb4ce2
susanm74 <susan.m.lister@gmail.com>
2023-01-17 16:41:58 +0700

    some attachment upload and attachment list updates
    - removed extraneous attachments compos from demo page
    - updated selectors and styles, still in progress
    - updated handle upload file methods to handle a single selected file, and then also to handle multiple dropped files, still in progress
    - added "jsx": "react-jsx" to tsconfig.json fix some import module errors

--- src/attachments/Attachments.tsx
+++ src/attachments/Attachments.tsx
@@ -16,20 +16,29 @@ export class Attachments extends FormElement {
     static get styles() {
         return css`
         .container {
-            min-height: 100px;
+            display: flex;
+            flex-direction: column;
+            flex-wrap: nowrap;
         }
 
-        #drag_and_drop_uploader {
+        .attachments {
+            width: 500px;
+        }
+
+        .uploader {
+            
+        }
+
+        .drag_and_drop {
             border: 2px dashed #ccc;
-            border-radius: 20px;
-            width: 480px;
+            // width: 480px;
             font-family: sans-serif;
             margin: 100px auto;
             padding: 20px;
         }
 
-        #drag_and_drop_uploader.highlight {
-            border-color: purple;
+        .drag_and_drop.highlight {
+            border-color: red;
         }
 
         .button {
@@ -45,7 +54,8 @@ export class Attachments extends FormElement {
             background: #ddd;
         }
 
-        #drag_and_drop_files {
+        #select_file,
+        #drop_files {
             display: none;
         }
     `;
@@ -54,14 +64,11 @@ export class Attachments extends FormElement {
     @property({ type: String })
     endpoint: string;
 
-    @property({ type: String, attribute: false })
-    classList: string;
-
     public constructor() {
         super();
     }
 
-    private fetchAttachments(){
+    private fetchAttachments(): void {
         const url = this.endpoint;
         if(url){
             const headers = {};
@@ -85,105 +92,112 @@ export class Attachments extends FormElement {
             .catch((error: any) => {
                 console.error(error);
             });
-        }        
+        }
     }
 
-    public refresh() {
+    public refresh(): void {
         this.fetchAttachments();
     }
 
-    public updated(changes: Map<string, any>) {
+    public updated(changes: Map<string, any>): void {
         super.updated(changes);
+        console.log('changes', changes);
 
         if (changes.has('endpoint')) {
             this.fetchAttachments();
         }
     }
 
-    private handleDragEnter(evt: DragEvent) {
+    private handleDragEnter(evt: DragEvent): void {
+        console.log('drag enter', evt);
         this.preventDefaults(evt);
         this.highlight(evt);
     }
 
-    private handleDragOver(evt: DragEvent) {
+    private handleDragOver(evt: DragEvent): void {
+        console.log('drag over', evt);
         this.preventDefaults(evt);
         this.highlight(evt);
 
     }
 
-    private handleDragLeave(evt: DragEvent) {
+    private handleDragLeave(evt: DragEvent): void {
+        console.log('drag leave', evt);
         this.preventDefaults(evt);
         this.unhighlight(evt);
 
     }
 
-    private handleDrop(evt: DragEvent) {
+    private handleDrop(evt: DragEvent): void {
+        console.log('drag drop', evt);
         this.preventDefaults(evt);
         this.unhighlight(evt);
 
         const dt = evt.dataTransfer;
         if (dt) {
             const files = dt.files;
-            const items = dt.items;
-            this.handleFiles(files);
+            this.uploadFiles(files);
         }
     }
 
-    private preventDefaults(evt: DragEvent) {
+    private preventDefaults(evt: DragEvent): void {
         evt.preventDefault();
         evt.stopPropagation();
     }
 
-    private highlight(evt: DragEvent) {
+    private highlight(evt: DragEvent): void {
         if (evt.target) {
             const dropArea = evt.target as HTMLDivElement;
             dropArea.classList.add('highlight');
         }
     }
 
-    private unhighlight(evt: DragEvent) {
+    private unhighlight(evt: DragEvent): void {
         if (evt.target) {
             const dropArea = evt.target as HTMLDivElement;
             dropArea.classList.add('active');
         }
     }
 
-    private handleFiles(files: FileList) {
-        console.log('handleFiles files', files);
-        const filesArray = [...files]
-        filesArray.map((file) => this.uploadFile(file))
+    private handleUploadFiles(evt: Event): void {
+        const target = evt.target as HTMLInputElement;
+        const files = target.files;
+        console.log('handleUploadFiles files', files);
+        [...files].map(file => this.uploadFile(file))
     }
 
-    private handleFile(file: File) {
-        console.log('handleFile file', file);
+    private uploadFiles(files: FileList): void {
+        console.log('uploadFiles files', files);
+        [...files].map(file => this.uploadFile(file))
+    }
+
+    private handleUploadFile(evt: Event): void {
+        const target = evt.target as HTMLInputElement;
+        const file = target.files[0];
+        console.log('handleUploadFile file', file);
         this.uploadFile(file);
     }
 
-    private uploadFile(file: File) {
+    private uploadFile(file: File): void {
         console.log('uploadFile file', file);
         // todo upload file - send request to RP endpoint
         const attachment = { name: file.name, url: file.name, type: file.type, size: file.size } as Attachment;
         this.addValue(attachment);
+        console.log('values', this.values);
         this.fireCustomEvent(CustomEventType.AttachmentUploaded, attachment);
     }
 
-    private handleRemove(attachment: Attachment) {
+    private handleRemoveFile(attachment: Attachment): void {
+        console.log('handleRemove attachment', attachment);
         // todo remove file - send request to RP endpoint
         this.removeValue(attachment);
+        console.log('values', this.values);
         this.fireCustomEvent(CustomEventType.AttachmentRemoved, attachment);
     }
 
-    // public render(): TemplateResult{
-    //     return html`
-    //         <div class="container">
-    //             Hello?
-    //         </div>`;
-    // }
-
     public render(): TemplateResult {
         return html`
-            <div class="container">
-                Hello?
+            <div class="container">                
                 <div class="attachments">
                     ${this.values.map(attachment => {
                         return html`
@@ -193,24 +207,25 @@ export class Attachments extends FormElement {
                                 ${attachment.url} 
                                 (${attachment.size}) 
                                 ${attachment.type}
-                                <temba-icon name="${Icon.delete_small}" class="clear-icon" @click=${this.handleRemove}/>
+                                <temba-icon name="${Icon.delete_small}" @click="${this.handleRemoveFile}"/>
                             </div>`
                     })}
                 </div>
-                <div id="vanilla_uploader">
-                    <input type="file" id="vanilla_file" onchange="${this.handleFile}">
-                    <label class="button" for="vanilla_file">Select a file</label>
-                </div>
-                <div id="drag_and_drop_uploader"
-                    class="${this.classList}"
-                    dragenter="${this.handleDragEnter}"
-                    dragover="${this.handleDragOver}"
-                    dragleave="${this.handleDragLeave}"
-                    drop="${this.handleDrop}"
+                <div class="attachments uploader">
+                    <input type="file" id="select_file" @change="${this.handleUploadFile}">
+                    <label class="button" for="select_file">Select a file</label>
+                </div> 
+                <div class="attachments uploader drag_and_drop ${this.classList}"
+                    @dragenter="${this.handleDragEnter}"
+                    @dragover="${this.handleDragOver}"
+                    @dragleave="${this.handleDragLeave}"
+                    @drop="${this.handleDrop}"
                 >
-                    <input type="file" id="drag_and_drop_files" multiple onchange="${this.handleFiles}">
-                    <label class="button" for="drag_and_drop_files">Select some files</label>
-                </div>
+                    <input type="file" id="drop_files" multiple @change="${this.handleUploadFiles}">
+                    <temba-icon name="${Icon.download}">
+                        <label class="button" for="drop_files">Drag and drop to attach files</label>
+                    </temba-icon>
+                </div>               
             </div>
         `;
     }
