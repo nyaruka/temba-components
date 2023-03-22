09b2b13b54c0f5e0d1d487a6765a65fb92e7f293
susanm74 <susan.m.lister@gmail.com>
2023-03-22 17:10:08 +0700

    - updated composeValue property with initial JSON value - minor update to deserializeComposeValue() to handle when this.value = '' - cleanup to setFocusOnChatbox() - updated handleChatboxChange() to use completion.value instead of textInput.value when setting this.currentText - removed extra setFocusOnChatbox() call in uploadFile() and handleSend() - removed extra console logs and comments - removed container click updates (those belong in a different branch)

--- src/contacts/ContactChat.ts
+++ src/contacts/ContactChat.ts
@@ -268,16 +268,16 @@ export class ContactChat extends ContactStoreElement {
       };
       const compose = evt.currentTarget as Compose;
       if (compose) {
-        const text = compose.currentText;
-        if (text.length > 0) {
+        const text = compose.composeValue.text;
+        if (text && text.length > 0) {
           payload['text'] = text;
         }
-        const attachments = compose.values.map(attachment => {
-          const content_type = attachment.content_type;
-          return content_type + ':' + attachment.url;
-        });
-        if (attachments.length > 0) {
-          payload['attachments'] = attachments;
+        const attachments = compose.composeValue.attachments;
+        if (attachments && attachments.length > 0) {
+          const attachment_uuids = attachments.map(
+            attachment => attachment.uuid
+          );
+          payload['attachments'] = attachment_uuids;
         }
       }
       if (this.currentTicket) {
