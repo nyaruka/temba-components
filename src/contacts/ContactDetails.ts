fcac45f9b41857ea0f96bc91bd586f0644b09393
Eric Newcomer <ericnewcomer@gmail.com>
2023-04-07 17:43:34 -0700

    Repurpose temba-contact-details for contact page

--- src/contacts/ContactDetails.ts
+++ src/contacts/ContactDetails.ts
@@ -1,349 +1,127 @@
 import { css, html, TemplateResult } from 'lit';
-import { property } from 'lit/decorators.js';
-import { Contact, Group, Ticket } from '../interfaces';
-import { RapidElement } from '../RapidElement';
-import { isDate, timeSince, truncate } from '../utils';
-import { Store } from '../store/Store';
-import { BODY_SNIPPET_LENGTH, fetchContact } from './helpers';
+import { ContactStoreElement } from './ContactStoreElement';
 import { Icon } from '../vectoricon';
+import { capitalize } from '../utils';
 
-export class ContactDetails extends RapidElement {
+const STATUS = {
+  active: 'Active',
+  blocked: 'Blocked',
+  stopped: 'Stopped',
+  archived: 'Archived',
+};
+
+const SCHEMES = {
+  tel: 'Phone',
+  whatsapp: 'WhatsApp',
+  fcm: 'Firebase Cloud Messaging',
+  twitter: 'Twitter',
+};
+
+export class ContactDetails extends ContactStoreElement {
   static get styles() {
     return css`
-      :host {
-        background: #f9f9f9;
-        display: block;
-        height: 100%;
-        position: relative;
-        overflow: hidden;
-        -webkit-mask-image: -webkit-radial-gradient(white, black);
+      .urn {
+        display: flex;
+        padding: 0.4em 1em 0.8em 1em;
+        border-bottom: 1px solid #e6e6e6;
+        margin-bottom: 0.5em;
       }
 
-      .contact {
-        display: flex;
-        flex-direction: column;
-        align-items: stretch;
-        height: 100%;
+      .urn .path {
+        margin-left: 0.2em;
       }
 
       .wrapper {
-        padding: 0em 1em;
-        flex-grow: 1;
-        display: flex;
-        flex-direction: column;
-      }
-
-      a {
-        color: var(--color-link-primary);
-      }
-
-      .field-links {
-        font-size: 0.8em;
-      }
-
-      .contact > .name {
-        font-size: 1.2em;
-        font-weight: 400;
-        padding: 0.5em 0.75em;
-        padding-right: 1em;
-      }
-
-      .group-label temba-icon {
-        display: inline-block;
-        fill: var(--color-text-dark);
-        margin-bottom: -2px;
-        margin-right: 4px;
-      }
-
-      .group-label {
-        box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1),
-          0 1px 2px 0 rgba(0, 0, 0, 0.06);
-        line-height: 1.25;
-        text-decoration: none;
-        cursor: default;
-        padding-left: 0.5rem;
-        padding-right: 0.5rem;
-        padding-top: 0.25rem;
-        padding-bottom: 0.25rem;
-        display: inline-block;
-        font-size: 0.75rem;
-        font-weight: 400;
-        border-radius: 9999px;
-        background-color: #f1f1f1;
-        color: rgba(0, 0, 0, 0.5);
-        letter-spacing: 0.025em;
-        white-space: nowrap;
-        text-align: center;
-        user-select: none;
-        -webkit-user-select: none;
-        margin-right: 0.75em;
-        margin-bottom: 0.75em;
+        padding-top: 0em;
       }
 
       .groups {
-        margin-top: 0.55em;
-        padding: 0px 0.75em;
-        margin-bottom: 0.3em;
+        padding: 0.4em 0.5em 0.6em 0.5em;
+        border-bottom: 1px solid #e6e6e6;
+        margin-bottom: 0.4em;
+      }
+      .group {
+        margin-right: 0.7em;
+        margin-bottom: 0.7em;
       }
 
-      .start-flow {
-      }
-
-      .actions {
-        margin-top: 16px;
-        border: 0px solid #ddd;
-        border-radius: var(--curvature);
-        padding: 0px;
-      }
-
-      .fields-wrapper {
-        background: #fff;
-        overflow: hidden;
-        margin: 0em -1em;
-
-        display: flex;
-        align-items: stretch;
-        flex-direction: column;
-        transition: all 300ms linear;
-      }
-
-      .fields-wrapper.expanded {
-        flex-grow: 2;
-      }
-
-      .body-wrapper {
-        overflow: hidden;
-      }
-
-      .body {
-        max-height: 200px;
-        overflow-y: auto;
-      }
-
-      .fields {
-        padding: 1em;
-        overflow-y: auto;
-      }
-
-      .fields-wrapper.expanded .fields {
-        flex-grow: 1;
-        height: 0px;
-      }
-
-      .field {
-        border-radius: var(--curvature);
-
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 0.3em;
-      }
-
-      .field .name {
-        margin-right: 8px;
-        font-weight: 400;
-        color: #666;
-        font-size: 0.9em;
-        word-break: break-word;
-      }
-      .field .value {
+      .label {
         font-size: 0.8em;
-        word-break: break-word;
-      }
-
-      temba-button {
-        margin-top: 5px;
-        display: block;
-        --button-y: 0;
+        color: rgb(136, 136, 136);
+        margin-left: 0.5em;
+        margin-bottom: 0.4em;
       }
     `;
   }
 
-  // optional display name
-  @property({ type: String })
-  name: string;
-
-  @property({ type: String })
-  uuid: string;
-
-  @property({ type: Object })
-  contact: Contact;
-
-  @property({ attribute: false })
-  flow: any = null;
-
-  // the fields with values for this contact
-  @property({ type: Array })
-  fields: string[] = [];
-
-  @property({ type: String })
-  endpoint: string;
-
-  @property({ type: Boolean })
-  expandFields = false;
-
-  @property({ type: Boolean })
-  expandBody = false;
-
-  @property({ type: Boolean })
-  showGroups = false;
-
-  @property({ type: Boolean })
-  showFlows = false;
-
-  @property({ type: Object })
-  ticket: Ticket = null;
-
-  public updated(changes: Map<string, any>) {
-    super.updated(changes);
-
-    if (changes.has('contact')) {
-      const store: Store = document.querySelector('temba-store');
-      if (this.contact && this.contact.fields) {
-        this.fields = Object.keys(this.contact.fields).filter((key: string) => {
-          const hasField = !!this.contact.fields[key];
-          return hasField && store.getContactField(key).featured;
-        });
-
-        this.contact.groups.forEach((group: Group) => {
-          group.is_dynamic = store.isDynamicGroup(group.uuid);
-        });
-
-        this.contact.groups.sort((a: Group, b: Group) => {
-          if (a.is_dynamic) {
-            return -1;
-          }
-          if (b.is_dynamic) {
-            return 1;
-          }
-          return a.name.localeCompare(b.name);
-        });
-      }
-    }
-
-    if (changes.has('endpoint')) {
-      this.flow = null;
-      this.expandFields = false;
-      fetchContact(this.endpoint).then((contact: Contact) => {
-        this.contact = contact;
-      });
-    }
-  }
-
-  private handleFlowChanged(evt: CustomEvent) {
-    this.flow = evt.detail.selected as any;
-  }
-
-  private handleToggleFields(): void {
-    this.expandFields = !this.expandFields;
-  }
-
-  private handleExpandBody(): void {
-    this.expandBody = true;
-  }
-
-  private handleHideBody(): void {
-    this.expandBody = false;
-  }
+  // Not sure if we want to include name here or not, so hold onto this for the moment
+  // ${this.data.name ? html`<temba-contact-field name="Name" value=${this.data.name} disabled></temba-contact-field>`:null}
 
   public render(): TemplateResult {
-    const store: Store = document.querySelector('temba-store');
-
-    let body = this.ticket ? this.ticket.body : null;
-    const showBodyToggle = body ? body.length > BODY_SNIPPET_LENGTH : false;
-    if (
-      !this.expandBody &&
-      this.ticket &&
-      this.ticket.body.length > BODY_SNIPPET_LENGTH
-    ) {
-      body = truncate(this.ticket.body, BODY_SNIPPET_LENGTH);
+    if (!this.data) {
+      return;
     }
 
-    if (this.contact) {
-      return html`<div class="contact">
-        ${this.ticket
-          ? html`<div class="name">${this.name || this.contact.name}</div>`
-          : null}
-        ${this.showGroups && !this.ticket
-          ? html`<div class="groups">
-              ${this.contact.groups.map((group: Group) => {
-                return html`
-                  <div
-                    onclick="goto(event)"
-                    href="/contact/filter/${group.uuid}/"
-                    class="group-label"
-                    style="cursor:pointer"
-                  >
-                    ${group.is_dynamic
-                      ? html`<temba-icon
-                          name="${Icon.group_smart}"
-                        ></temba-icon>`
-                      : null}${group.name}
-                  </div>
-                `;
+    const lang = this.store.getLanguageName(this.data.language);
+
+    return html`
+      <div class="wrapper">
+        ${this.data.groups.length > 0
+          ? html` <div class="groups">
+              <div class="label">Groups</div>
+              ${this.data.groups.map(group => {
+                return html`<temba-label
+                  class="group"
+                  onclick="goto(event)"
+                  href="/contact/filter/${group.uuid}/"
+                  icon=${group.is_dynamic ? Icon.group_smart : Icon.group}
+                  clickable
+                  shadow
+                >
+                  ${group.name}
+                </temba-label>`;
               })}
             </div>`
-          : html``}
+          : null}
+        ${this.data.urns.map(urn => {
+          const parts = urn.split(':');
+          let scheme = SCHEMES[parts[0]];
+          if (!scheme) {
+            scheme = capitalize(parts[0] as any);
+          }
+          return html`<temba-contact-field
+            name=${scheme}
+            value=${parts[1]}
+            disabled
+          ></temba-contact-field>`;
+        })}
 
-        <div class="wrapper">
-          ${body
-            ? html`<div class="body-wrapper">
-                <div class="body">${body}</div>
-                <div class="field-links">
-                  ${showBodyToggle
-                    ? !this.expandBody
-                      ? html`<a href="#" @click="${this.handleExpandBody}"
-                          >more</a
-                        >`
-                      : html`<a href="#" @click="${this.handleHideBody}"
-                          >less</a
-                        >`
-                    : null}
-                </div>
-              </div>`
-            : null}
-          ${this.fields.length > 0
-            ? html` <div
-                  class="fields-wrapper ${this.expandFields ? 'expanded' : ''}"
-                >
-                  <div class="fields">
-                    ${this.fields
-                      .slice(0, this.expandFields ? 255 : 3)
-                      .map((key: string) => {
-                        let value = this.contact.fields[key];
-                        if (value) {
-                          if (isDate(value)) {
-                            value = timeSince(new Date(value));
-                          }
-                          return html`<div class="field">
-                            <div class="name">
-                              ${store.getContactField(key).label}
-                            </div>
-                            <div class="value">${value}</div>
-                          </div>`;
-                        }
-                      })}
-                  </div>
-                </div>
-                <div style="display:flex;">
-                  <div style="flex-grow:1"></div>
-                  <div style="margin-right:1em;margin-top:-0.5em">
-                    ${this.fields.length > 3
-                      ? html`<temba-icon
-                          name="${this.expandFields
-                            ? Icon.arrow_up
-                            : Icon.arrow_down}"
-                          @click=${this.handleToggleFields}
-                          animateChange="spin"
-                          circled
-                          clickable
-                        ></temba-icon>`
-                      : null}
-                  </div>
-                </div>`
-            : null}
-          <div style="flex-grow:1"></div>
-        </div>
-      </div>`;
-    }
+        <temba-contact-field
+          name="Status"
+          value=${STATUS[this.data.status]}
+          disabled
+        ></temba-contact-field>
+        ${lang
+          ? html`<temba-contact-field
+              name="Language"
+              value=${lang}
+              disabled
+            ></temba-contact-field>`
+          : null}
+
+        <temba-contact-field
+          name="Created"
+          value=${this.data.created_on}
+          type="datetime"
+          disabled
+        ></temba-contact-field>
+        <temba-contact-field
+          name="Last Seen"
+          value=${this.data.last_seen_on}
+          type="datetime"
+          disabled
+        ></temba-contact-field>
+      </div>
+    `;
   }
 }
