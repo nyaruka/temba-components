826a015faa4ecbd581b253dbf74dc01f22490333
Eric Newcomer <ericnewcomer@gmail.com>
2024-10-31 21:22:02 +0000

    Remove fill color from empty coffee

--- src/vectoricon/index.ts
+++ src/vectoricon/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-duplicate-enum-values */
 // for cache busting we dynamically generate a fingerprint, use yarn svg to update
-export const SVG_FINGERPRINT = 'dc1b40c6715b2a89b3399538a24fc5a2';
+export const SVG_FINGERPRINT = '295f7cda9fe74431021b5d198282ae96';
 
 // only icons below are included in the sprite sheet
 export enum Icon {
