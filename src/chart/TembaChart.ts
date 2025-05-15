f10e475fcc3dc316708340686b0d7f235cf918e7
Eric Newcomer <ericnewcomer@gmail.com>
2025-05-15 15:02:10 +0000

    Fix bad merge

--- src/chart/TembaChart.ts
+++ src/chart/TembaChart.ts
@@ -3,7 +3,8 @@ import { property } from 'lit/decorators.js';
 import { html, TemplateResult } from 'lit';
 import { Chart, ChartType } from 'chart.js/auto';
 
-/* const colors = [
+/*
+const colors = [
   'rgba(255, 99, 132, 0.2)',
   'rgba(255, 159, 64, 0.2)',
   'rgba(255, 205, 86, 0.2)',
@@ -36,43 +37,74 @@ export class TembaChart extends RapidElement {
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
-    this.shadowRootDiv = document.createElement('div');
-    this.canvas = document.createElement('canvas');
-    this.canvas.setAttribute('width', '100%');
-    this.canvas.setAttribute('height', '30px');
-    this.shadowRootDiv.append(this.canvas);
-    this.shadowRoot.appendChild(this.shadowRootDiv);
   }
 
-  protected render(): TemplateResult {
-    // const data = JSON.parse(this.getAttribute('data'));
+  connectedCallback(): void {
+    super.connectedCallback();
+
+    this.shadowRootDiv = document.createElement('div');
+    this.canvas = document.createElement('canvas');
+    this.canvas.setAttribute('height', '300px');
+    this.shadowRootDiv.append(this.canvas);
+    this.shadowRoot.appendChild(this.shadowRootDiv);
+
+    this.ctx = this.canvas.getContext('2d');
+    this.createChart();
+  }
+
+  private createChart(): void {
     const data = {
       type: this.chartType,
       data: {
-        labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        labels: [
+          '3/4/2025',
+          '3/5/2025',
+          '3/6/2025',
+          '3/7/2025',
+          '3/8/2025',
+          '3/9/2025',
+          '3/10/2025',
+          '3/11/2025',
+          '3/12/2025',
+          '3/13/2025',
+          '3/14/2025'
+        ],
         datasets: [
           {
-            label: 'dataset 1',
-            data: [5, 9, 3, 5, 2, 6, 1, 4, 7, 8, 0],
-            borderWidth: 1,
-            backgroundColor: ['rgba(54, 162, 235, 0.2)'],
-            borderColor: ['rgb(54, 162, 235)']
+            label: 'General',
+            data: [65, 9, 23, 15, 20, 68, 19, 44, 97, 28, 30]
+          },
+          {
+            label: 'VIPs',
+            data: [5, 19, 3, 52, 10, 58, 9, 14, 37, 29, 39]
+          },
+          {
+            label: 'Support',
+            data: [29, 65, 45, 5, 2, 6, 31, 54, 67, 48, 25]
           }
         ]
       },
       options: {
+        responsive: true,
+        maintainAspectRatio: false,
         scales: {
           y: {
-            beginAtZero: true
+            stacked: true
+          },
+          x: {
+            stacked: true
           }
         }
       }
     };
 
-    window.setTimeout(() => {
-      const context = this.canvas.getContext('2d');
-      this.chart = new Chart(context, data);
-    }, 0);
+    this.chart = new Chart(this.ctx, data);
+  }
+
+  protected render(): TemplateResult {
+    // const data = JSON.parse(this.getAttribute('data'));
+
+    // window.setTimeout(() => {}, 0);
 
     return html``;
   }
