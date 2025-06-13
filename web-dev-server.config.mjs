c363c6047b6ab4d1a3a56cccc5d8ee5424dd1674
Eric Newcomer <ericnewcomer@gmail.com>
2025-06-13 18:22:16 +0000

    Add flow example

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,5 +1,7 @@
 import replace from '@rollup/plugin-replace';
 import { fromRollup } from '@web/dev-server-rollup';
+import fs from 'fs';
+import path from 'path';
 
 const replacePlugin = fromRollup(replace);
 
@@ -10,5 +12,21 @@ export default {
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('development'),
     }),
+  ],
+
+  middlewares: [
+    async (ctx, next) => {
+      if (ctx.path.startsWith('/flow/revisions/')) {
+        const parts = ctx.path.split('/');
+        const uuid = parts[3];
+        ctx.set('Content-Type', 'application/json');
+        ctx.body = fs.readFileSync(
+          path.resolve(`./demo/data/flows/${uuid}.json`),
+          'utf-8',
+        );
+      } else {
+        await next();
+      }
+    },
   ],
 };
