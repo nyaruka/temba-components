fc561d46a680266e97a94ebf5dec6599780890fe
Eric Newcomer <ericnewcomer@gmail.com>
2026-07-23 15:27:18 -0700

    format and build artifacts

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -3,7 +3,7 @@
     // Re-generate this file by running lit-localize
 
     
-    
+    import {str} from '@lit/localize';
 
     /* eslint-disable no-irregular-whitespace */
     /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -12,7 +12,7 @@
       'scf1453991c986b25': `Tab para completar, enter para seleccionar`,
 's73b4d70c02f4b4e0': `No options`,
 'sbc913d7dc0f33877': `to add`,
-'saa8939ac88a76f98': `Last seen`,
+'s7722a91d3a512442': str`Last seen ${0}`,
 's22965bb9808befb0': `Interrupt`,
 's122d4de68bcfcdf4': `It's okay to restart`,
 's3eb2567092b4d7c1': `from the beginning`,
