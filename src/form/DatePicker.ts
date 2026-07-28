75033c4de81635390bc1d6c7a5e4164ce61858d3
Eric Newcomer <eric@textit.com>
2026-07-28 10:07:25 -0700

    Fill full width with timezone box when datepicker wraps (#1079)

--- src/form/DatePicker.ts
+++ src/form/DatePicker.ts
@@ -29,7 +29,7 @@ export class DatePicker extends FieldElement {
 
       .input-wrapper {
         padding: var(--temba-textinput-padding);
-        flex-grow: 1;
+        flex-grow: 999;
       }
 
       .tz {
@@ -65,6 +65,7 @@ export class DatePicker extends FieldElement {
         flex-direction: row;
         align-items: center;
         padding: 0.4em 0em;
+        flex-grow: 1;
       }
 
       .container:focus-within {
